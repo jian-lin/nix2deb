93e324b3fbf1b905fa4cecdf0a58203af4de1b11
Lin Jian <me@linj.tech>
2025-05-12 19:39:44 +0800

    Log: do not show source location when log level >= Info

--- app/Main/Log.hs
+++ app/Main/Log.hs
@@ -3,11 +3,20 @@ module Main.Log
   )
 where
 
-import Colog (LogAction, Message, Severity)
+import Colog (LogAction, Message, Msg (..), Severity)
 import Colog qualified as Log
 import Relude
 
 mkLogAction :: (MonadIO m) => Severity -> LogAction m Message
-mkLogAction severity = Log.filterBySeverity severity Log.msgSeverity Log.simpleMessageAction
+mkLogAction severity = Log.filterBySeverity severity Log.msgSeverity (logAction severity)
 
--- TODO change format to exclude filename#line?
+-- | Action that prints `Message` to `stderr`.
+logAction :: (MonadIO m) => Severity -> LogAction m Message
+logAction severity = encodeUtf8 . fmtMessage severity >$< Log.logByteStringStderr
+{-# INLINE logAction #-}
+{-# SPECIALIZE logAction :: Severity -> LogAction IO Message #-}
+
+fmtMessage :: Severity -> Message -> Text
+fmtMessage severity message@Msg {msgSeverity, msgText}
+  | severity <= Log.Debug = Log.fmtMessage message
+  | otherwise = Log.showSeverity msgSeverity <> msgText
